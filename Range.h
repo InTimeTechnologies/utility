d24542c21caa08d2c015afd91b473dd74fdee47f
Christopher Barrios The Man <68883351+InTimeTechnologies@users.noreply.github.com>
2025-08-03 02:59:04 -0400

    Update Range.h

--- Range.h
+++ Range.h
@@ -1,5 +1,3 @@
-#pragma once
-
 /******************************************************************************
  * Filename:    Range.h
  * Author:      Christophr Barrios Agosto
@@ -18,53 +16,90 @@
  * License:     MIT License
  ******************************************************************************/
 
-template <typename T> struct Range {
-	// Properties
-	T x0{};
-	T x1{};
+#pragma once
 
-	// Constructor / Destructor
-	Range() = default;
-	Range(T value) : x0(value), x1(value) {}
-	Range(T x0, T x1) : x0(x0), x1(x1) {}
+// Forward declarations
+template <typename T> class BinaryRangeTree;
 
-	// Getters
-	T getRange() const {
-		return x1 - x0;
-	}
+template <typename T> class Range {
+	// Friends
+	friend class BinaryRangeTree<T>;
 
-	// Operators | comparison
-	bool operator==(const Range& other) const {
-		return x0 == other.x0 && x1 == other.x1;
-	}
-	bool operator!=(const Range& other) const {
-		return x0 != other.x0 || x1 != other.x1;
-	}
-	bool operator<(const Range& other) const {
-		return x1 < other.x0;
-	}
-	bool operator>(const Range& other) const {
-		return x0 > other.x1;
-	}
-	bool operator<=(const Range& other) const {
-		return x1 <= other.x0;
-	}
-	bool operator>=(const Range& other) const {
-		return x0 >= other.x1;
-	}
+	// Object
+	private:
+		// Properties
+		T x0{};
+		T x1{};
 
-	// Functions
-	bool overlaps(const Range& other) const {
-		return !(x1 < other.x0 || x0 > other.x1);
-	}
-	bool inside(const Range& other) const {
-		return x0 <= other.x1 && x1 >= other.x0;
-	}
-	void merge(const Range& other) {
-		x0 = x0 < other.x0 ? x0 : other.x0;
-		x1 = x1 > other.x1 ? x1 : other.x1;
-	}
-	Range getMerge(const Range& other) const {
-		return Range{ x0 < other.x0 ? x0 : other.x0, x1 > other.x1 ? x1 : other.x1 };
-	}
+	public:
+		// Constructor / Destructor
+		Range() = default;
+		Range(T value) : x0(value), x1(value) {
+			if (x0 > x1)
+				std::swap(x0, x1);
+		}
+		Range(T x0, T x1) : x0(x0), x1(x1) {
+			if (this->x0 > this->x1)
+				std::swap(this->x0, this->x1);
+		}
+
+		// Getters
+		T getX0() const {
+			return x0;
+		}
+		T getX1() const {
+			return x1;
+		}
+		T getRange() const {
+			return x1 - x0;
+		}
+
+		// Setters
+		void setX0(T x0) {
+			if (x0 > x1)
+				x1 = x0;
+			else
+				this->x0 = x0;
+		}
+		void setX1(T x1) {
+			if (x1 < x0)
+				x0 = x1;
+			else
+				this->x1 = x1;
+		}
+
+		// Operators | comparison
+		bool operator==(const Range& other) const {
+			return x0 == other.x0 && x1 == other.x1;
+		}
+		bool operator!=(const Range& other) const {
+			return x0 != other.x0 || x1 != other.x1;
+		}
+		bool operator<(const Range& other) const {
+			return x1 < other.x0;
+		}
+		bool operator>(const Range& other) const {
+			return x0 > other.x1;
+		}
+		bool operator<=(const Range& other) const {
+			return x1 <= other.x0;
+		}
+		bool operator>=(const Range& other) const {
+			return x0 >= other.x1;
+		}
+
+		// Functions
+		bool overlaps(const Range& other) const {
+			return !(x1 < other.x0 || x0 > other.x1);
+		}
+		bool inside(const Range& other) const {
+			return x0 <= other.x1 && x1 >= other.x0;
+		}
+		void merge(const Range& other) {
+			x0 = x0 < other.x0 ? x0 : other.x0;
+			x1 = x1 > other.x1 ? x1 : other.x1;
+		}
+		Range getMerge(const Range& other) const {
+			return Range{ x0 < other.x0 ? x0 : other.x0, x1 > other.x1 ? x1 : other.x1 };
+		}
 };
